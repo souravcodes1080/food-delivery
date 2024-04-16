811a520ef1b403c2c909a1e258b517c2fa3c183b
SouravCodes1080 <107361153+souravcodes1080@users.noreply.github.com>
2024-04-16 23:15:41 +0530

    menu

--- src/components/ExploreMenu/ExploreMenu.jsx
+++ src/components/ExploreMenu/ExploreMenu.jsx
@@ -1,18 +1,28 @@
-import React from 'react'
-import './exploreMenu.css'
-import {menu_list} from '../../assets/assets'
+import React from "react";
+import "./exploreMenu.css";
+import { menu_list } from "../../assets/assets";
 function ExploreMenu() {
   return (
     <>
-        <div className="explore-menu" id='explore-menu'>
-            <h1>Explore our menu</h1>
-            <p className='explore-menu-text'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quas ut enim incidunt fugiat illo voluptates, porro expedita nostrum ducimus maiores.</p>
-            <div className="explore-menu-list">
-                
+      <div className="explore-menu" id="explore-menu">
+        <h1>Explore our menu</h1>
+        <p className="explore-menu-text">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quas ut enim
+          incidunt fugiat illo voluptates, porro expedita nostrum ducimus
+          maiores.
+        </p>
+        <div className="explore-menu-list">
+          {menu_list.map((item, index) => (
+            <div key={index} className="explore-menu-list-item">
+              <img src={item.menu_image} alt="menu_image" />
+              <p>{item.menu_name}</p>
             </div>
+          ))}
         </div>
+        <hr />
+      </div>
     </>
-  )
+  );
 }
 
-export default ExploreMenu
+export default ExploreMenu;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./home.css";
 import Header from "../../components/Header/Header";
+import ExploreMenu from "../../components/ExploreMenu/ExploreMenu";
 function Home() {
   return (
     <>
-        <Header />
+      <Header />
+      <ExploreMenu />
     </>
   );
 }
