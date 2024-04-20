6f623ecdca34f0a373130673b30457df98998790
SouravCodes1080 <107361153+souravcodes1080@users.noreply.github.com>
2024-04-20 19:34:28 +0530

    mongodb

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,10 @@
 import mongoose from "mongoose";
 
 export const connectDb = async ()=>{
+    if (!process.env.MONGODB_URI) {
+        console.error('MongoDB URI is not defined. Please check your environment variables.');
+        process.exit(1); // Exit the application process
+    }
     await mongoose.connect(process.env.MONGODB_URI)
     .then(()=>console.log("Connected to Database"))
 }
