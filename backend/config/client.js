ac8d471aa56520880a753ac4d719cc48b6a52d61
SouravCodes1080 <107361153+souravcodes1080@users.noreply.github.com>
2024-04-26 08:19:57 +0530

    domains

--- backend/config/client.js
+++ backend/config/client.js
@@ -1,2 +1,2 @@
-export const CLIENT_DOMAIN = "http://localhost:5173";
-// export const CLIENT_DOMAIN = 'https://tomato-food-delivery.onrender.com'
+//export const CLIENT_DOMAIN = "http://localhost:5173";
+export const CLIENT_DOMAIN = 'https://tomato-food-delivery.onrender.com'
--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,2 +1,2 @@
-export const DOMAIN = 'http://localhost:8080'
-// export const DOMAIN = 'https://food-delivery-backend-cu8q.onrender.com'
+//export const DOMAIN = 'http://localhost:8080'
+export const DOMAIN = 'https://food-delivery-backend-cu8q.onrender.com'
