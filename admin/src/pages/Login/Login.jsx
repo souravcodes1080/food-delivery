4ff3d878629f7525fbaeadf66f5b8ef79354aea4
SouravCodes1080 <107361153+souravcodes1080@users.noreply.github.com>
2024-07-12 19:04:06 +0530

    admin panel responsive css

--- admin/src/pages/Login/Login.jsx
+++ admin/src/pages/Login/Login.jsx
@@ -6,7 +6,7 @@ function Login() {
     <>
     <div className="main">
 
-      <img src={assets.hero} alt='hero_img'/>
+      <img src={assets.hero} alt='hero_img' width={"100%"}/>
     </div>
     </>
   )
