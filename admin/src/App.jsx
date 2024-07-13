b2302303979d88b7bba718ac66a4a522f09650ab
SouravCodes1080 <107361153+souravcodes1080@users.noreply.github.com>
2024-07-13 18:19:01 +0530

    security check

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,7 +1,7 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "./components/Navbar/Navbar";
 import Sidebar from "./components/Sidebar/Sidebar";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useNavigate } from "react-router-dom";
 import Add from "./pages/Add/Add";
 import List from "./pages/List/List";
 import Orders from "./pages/Orders/Orders";
@@ -9,25 +9,45 @@ import Update from "./pages/Update/Update";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Login from "./pages/Login/Login";
+
 function App() {
+  const [isAuthorized, setIsAuthorized] = useState(false);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const passcode = localStorage.getItem("passcode");
+    if (passcode === "TAYLORSWIFT13") {
+      setIsAuthorized(true);
+    } else {
+      setIsAuthorized(false);
+      alert("Unauthorized access. Please enter the correct passcode.");
+      navigate("/");
+    }
+  }, [navigate]);
+
   return (
     <>
       <ToastContainer />
-      
-      <Navbar />
-      <hr />
-      <div className="app-content">
-        <Sidebar />
-        <div className="inner-body">
-          <Routes>
-        <Route path="/"  exact element={<Login />} />
-            <Route path="/add" element={<Add />} />
-            <Route path="/list" element={<List />} />
-            <Route path="/orders" element={<Orders />} />
-            <Route path="/update/:id" element={<Update />} />
-          </Routes>
-        </div>
-      </div>
+      {isAuthorized ? (
+        <>
+          <Navbar />
+          <hr />
+          <div className="app-content">
+            <Sidebar />
+            <div className="inner-body">
+              <Routes>
+                <Route path="/" exact element={<Login />} />
+                <Route path="/add" element={<Add />} />
+                <Route path="/list" element={<List />} />
+                <Route path="/orders" element={<Orders />} />
+                <Route path="/update/:id" element={<Update />} />
+              </Routes>
+            </div>
+          </div>
+        </>
+      ) : (
+        <Login />
+      )}
     </>
   );
 }
